e72c15947d4bd38929af73cac36d933c26c8efcc
JaeYooooon <dhy04050@naver.com>
2023-05-31 19:27:43 +0900

    feat: [003-EMAIL-VERIFICATION] 커스터머 이메일 verification.

--- user-api/src/main/java/com/zerobase/cms/user/client/MailgunClient.java
+++ user-api/src/main/java/com/zerobase/cms/user/client/MailgunClient.java
@@ -4,11 +4,12 @@ import com.zerobase.cms.user.client.mailgun.SendMailForm;
 import feign.Response;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient(name = "mailgun", url = "https://api.mailgun.net/v3/")
 @Qualifier("mailgun")
 public interface MailgunClient {
     @PostMapping("sandbox4dfd4ad6d7c24519bbfa70c1a67075eb.mailgun.org/messages")
-    Response sendEmail(@SpringQueryMap SendMailForm form);
+    ResponseEntity<String> sendEmail(@SpringQueryMap SendMailForm form);
 }
