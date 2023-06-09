04083c71fd247429d187707446403232ce2116eb
JaeYooooon <dhy04050@naver.com>
2023-06-09 12:59:56 +0900

    feat : [013-add-cart] Cart 에 대한 추가 기능 구현

---
+++ order-api/src/main/java/com/zerobase/cms/order/domain/product/AddProductCartForm.java
@@ -0,0 +1,29 @@
+package com.zerobase.cms.order.domain.product;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddProductCartForm {
+    private Long Id;
+    private Long sellerId;
+    private String name;
+    private String description;
+    private List<ProductItem> items;
+
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ProductItem{
+        private Long id;
+        private String name;
+        private Integer count;
+        private Integer price;
+    }
+}
